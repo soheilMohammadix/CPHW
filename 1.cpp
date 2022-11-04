014f6ca8492c52404b65deb73eea35f8c006a098
soheilmohammadi1 <116627480+soheilmohammadi1@users.noreply.github.com>
2022-11-04 19:59:22 +0330

    4th week homework

---
+++ 1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    float a, b;
+    cout << "first number : ";
+    cin >> a;
+    cout << "second number : ";
+    cin >> b;
+    cout << "a + b = " << a + b;
+    cout << "\na - b = " << a - b;
+    cout << "\na * b = " << a * b;
+    cout << "\na % b = " << (int) a % (int) b;
+
+
+}
---
+++ 2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    float x, y, z;
+    int result0, result1;
+    cout << "Please enter x : ";
+    cin >> x;
+    cout << "Please enter y : ";
+    cin >> y;
+    cout << "Please enter z : ";
+    cin >> z;
+    result0 = (x - y) * (x + z) ;
+    result1 = pow(x + y + z, 2) + 1;
+    cout << result0 / result1;
+}
