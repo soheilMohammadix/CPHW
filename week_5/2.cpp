ba9f716b198688ce3d9681811da056bf19fadbb8
soheilmohammadi1 <116627480+soheilmohammadi1@users.noreply.github.com>
2022-11-11 12:04:29 +0330

    Create 2.cpp

---
+++ week_5/2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	char character;
+	cin>> character;
+	switch (character){
+		case 'a':
+			cout << "VOWEL";
+			break;
+		case 'i':
+			cout << "VOWEL";
+			break;
+		case 'o':
+			cout << "VOWEL";
+			break;
+		case 'e':
+			cout << "VOWEL";
+			break;
+		case 'u':
+			cout << "VOWEL";
+			break;
+		default:
+			cout << "CONSONANT";
+	}
+	
+}
